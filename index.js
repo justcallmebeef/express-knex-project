a141e4fa8c66193a281eab4ac6d49dadee906d40
justcallmebeef <bethanymitch10@gmail.com>
2018-11-27 13:13:42 -0700

    added a database connection, wrote a queries to grab student data

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express') 
 const app = express()
+const queries = require('./queries')
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.send('done did it.')
+    queries.listAll().then(students => res.send(students))
 })
 
 app.listen(port, console.log(`listening on port ${port}`))
