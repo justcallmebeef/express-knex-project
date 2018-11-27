815fbcb362f328a1499b30dee4cb540e967f1cd0
justcallmebeef <bethanymitch10@gmail.com>
2018-11-27 11:07:10 -0700

    added data into seeds and it ran correctly

--- seeds/01_students.js
+++ seeds/01_students.js
@@ -1,12 +1,25 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('students').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('students').insert([
+        {
+          name: 'Grant Fogle',
+          fave_animal: 'https://media.mnn.com/assets/images/2017/01/sleeping-bear-log.jpg.838x0_q80.jpg',
+          previous_occupation: 'SEO Account Dude',
+          hometown_lat: 39.5186,
+          hometown_long: 104.7614,
+          useless_superpower: 'always 5 minutes late'
+        },
+        {
+          name: "Kate",
+          fave_animal: "https://images.pexels.com/photos/53001/cougar-mountain-lion-puma-concolor-big-cat-53001.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+          previous_occupation: "Event Manager",
+          hometown_lat: 35.0844,
+          hometown_long: 106.6504,
+          useless_superpower: "Can read the minds of houseflies"
+        }
       ]);
     });
 };
