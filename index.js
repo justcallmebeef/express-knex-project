b4beea54e6de0afdf5f70758c62fea8d1a7b1901
justcallmebeef <bethanymitch10@gmail.com>
2018-11-28 10:00:53 -0700

    added a query by id in queries and a get route in index

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express') 
 const app = express()
 const queries = require('./queries')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const port = process.env.PORT || 3000
+
+app.use(bodyParser.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
     queries.listAll().then(students => res.send(students))
+})
+
+app.get('/:id', (req, res) => {
+    const id = req.params.id
+    queries.getById(id).then(response => res.send(response))
 })
 
 app.listen(port, console.log(`listening on port ${port}`))
