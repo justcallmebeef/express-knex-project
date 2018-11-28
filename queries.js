6ddb5d4fc05de275c7fc2ee5c1a8b014519e2900
justcallmebeef <bethanymitch10@gmail.com>
2018-11-28 10:09:48 -0700

    updated queries to include different ways to write the getbyid query

--- queries.js
+++ queries.js
@@ -6,9 +6,9 @@ module.exports = {
     },
 
     getById(id) {
-        return database('students').where('id', id)
-        // Below does the same thing
-        // return database('students').where({id: id}).first() 
+        return database('students').where({id: id}).first() 
+        // Below does the same thing - I used .first() because it returns an object vs. an id
+        // return database('students').where('id', id)
         // return database.select().from('students').where('id', id)
     }
 }
